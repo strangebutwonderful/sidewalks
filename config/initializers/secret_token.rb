43c6332488a63d225819c5cbada780c52cee11a9
Ajay Sharma <aj@ajsharma.com>
2013-06-22 11:46:47 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sidewalks::Application.config.secret_token = '124e54d4df74429c37714a4030afce4db79ab01d9692e4baf347926ebf309d1c5d9d8a62c782620dbe941ed6f203ed4eeb2e3350386648f15087106979e4bda9'
