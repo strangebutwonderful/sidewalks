a9163f44ff6194dc9b705fc9d082adb507b730c6
Ajay Sharma <aj@ajsharma.com>
2013-07-05 13:33:40 -0700

    Order by created at desc

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order("created_at DESC").all
 
     respond_to do |format|
       format.html # index.html.erb
