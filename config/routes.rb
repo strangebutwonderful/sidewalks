992bda214f95d25f5b7381da5b7a17a4d8c8c93d
Ajay Sharma <aj@ajsharma.com>
2013-09-17 12:30:24 -0700

    Allow import to be called on demand without needing to hard link

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Sidewalks::Application.routes.draw do
   
-  match '/tweets/import'  => 'tweets#import'
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
   match '/auth/failure' => 'sessions#failure'
   match '/welcome' => 'home#welcome'
 
-  resources :tweets
+  resources :tweets do 
+    collection do 
+      get 'import'     
+    end
+  end
+
   resources :users, :only => [:index, :show, :edit, :update ]
 
   root :to => "tweets#index"
