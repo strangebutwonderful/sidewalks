0cc527975f5324acbc95e9d28f9776731a1664e4
Ajay Sharma <aj@ajsharma.com>
2013-09-23 10:56:19 -0700

    Removing secret token from source control, whoops

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sidewalks::Application.config.secret_token = '2852f0b03d75ac42629452a47c74a204586ae7ef0da9b4b9d2e1fc1bcc184d4b60ce94a8eefa0cd2f141b2571e212e46363de9e7bfb74d80d35aec3bb5e0f880'
+Sidewalks::Application.config.secret_token = ENV['SECRET_TOKEN']
