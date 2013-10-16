2ecb8a58b3d39a94ae2f9027ea09e1ba3cb13213
Ajay Sharma <aj@ajsharma.com>
2013-10-15 22:39:56 -0700

    Move initializer to config

--- app/models/twitter_noise_importer.rb
+++ app/models/twitter_noise_importer.rb
@@ -1,14 +1,5 @@
 class TwitterNoiseImporter
 
-  def self.configure 
-    Twitter.configure do |config|
-      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-      config.oauth_token = ENV['TWITTER_OAUTH_ACCESS_TOKEN']
-      config.oauth_token_secret = ENV['TWITTER_OAUTH_ACCESS_TOKEN_SECRET']
-    end
-  end
-
   def self.import_latest_from_sidewalks_twitter 
     # Import the latest noises from twitter adn saves to db
 
@@ -19,8 +10,6 @@ class TwitterNoiseImporter
   end
 
   def self.latest_noises_from_sidewalks_twitter
-    self.configure
-
     last_noise = Noise.last
 
     if last_noise && last_noise.provider_id
