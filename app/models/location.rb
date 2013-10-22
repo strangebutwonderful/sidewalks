5e5130a48b073fb10b26e85fbc8533aafbf0079d
Ajay Sharma <aj@ajsharma.com>
2013-10-22 11:42:48 -0700

    Annotations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  address    :string(255)      not null
+#  city       :string(255)      not null
+#  state      :string(255)      not null
+#  zip        :integer          not null
+#  latitude   :decimal(11, 8)
+#  longitude  :decimal(11, 8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Location < ActiveRecord::Base
   belongs_to :user
   attr_accessible :user_id, :address, :city, :latitude, :longitude, :state, :zip
--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  address    :string(255)      not null
+#  city       :string(255)      not null
+#  state      :string(255)      not null
+#  zip        :integer          not null
+#  latitude   :decimal(11, 8)
+#  longitude  :decimal(11, 8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
