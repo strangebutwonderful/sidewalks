c0954b36f924593da502dac20c904d28cc99113f
Ajay Sharma <aj@ajsharma.com>
2013-10-30 22:22:10 -0700

    Adding user controller test

---
+++ test/functional/users_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = FactoryGirl.create(:user)
+  end
+
+  test "user should not show user" do
+    sign_in(FactoryGirl.create(:admin_user))
+
+    get :show, id: @user
+    assert_response :success
+  end
+
+  test "admin should get index" do    
+    sign_in(FactoryGirl.create(:admin_user))
+
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "user should not get edit" do
+    sign_in(FactoryGirl.create(:user))
+
+    get :edit, id: @user
+    assert_redirected_to :root
+  end
+
+  test "admin should get edit" do
+    sign_in(FactoryGirl.create(:admin_user))
+
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "user should not update user" do
+    sign_in(FactoryGirl.create(:user))
+
+    put :update, id: @user, user: { provider: @user.provider, provider_id: @user.provider_id }
+    assert_redirected_to :root
+  end
+
+  test "admin should update user" do
+    sign_in(FactoryGirl.create(:admin_user))
+
+    put :update, id: @user, user: { provider: @user.provider, provider_id: @user.provider_id }
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+end
