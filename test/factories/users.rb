d912d4766bfe06d221fcfae8575522674f749b31
Ajay Sharma <aj@ajsharma.com>
2013-11-06 12:29:18 -0800

    More unique factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,12 +1,16 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :provider_id do |n|
+    "MyProviderId#{n}"
+  end
+
   factory :user do
-    name "MyString"
+    name Faker::Name.name
     email "MyString@example.com"
     provider "MyString"
-    provider_id "MyString"
-    provider_screen_name "MyString"
+    provider_id
+    provider_screen_name Faker::Internet.user_name
   end
 
   factory :admin_user, parent: :user do
