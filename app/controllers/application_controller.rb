ecea10c49802ea1f140599c019e34ff94be7cb31
Ajay Sharma <aj@ajsharma.com>
2013-11-27 12:48:11 -0800

    Record exception for current user if occurs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,8 @@ class ApplicationController < ActionController::Base
     def current_user
       begin
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      rescue Exception => e
+      rescue Exception => exception
+        Rails.logger.error exception
         nil
       end
     end
