3633233b2249399c73ce156a70fe54373e2f4a38
Ajay Sharma <aj@ajsharma.com>
2013-11-27 13:27:21 -0800

    Add some noises to ensure realistic test

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -6,7 +6,12 @@ class BrowsingTest < ActionDispatch::PerformanceTest
   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
   #                          :output => 'tmp/performance', :formats => [:flat] }
 
+  def setup 
+    FactoryGirl.create(:noise, 500)
+  end
+
   def test_homepage
     get '/'
   end
+  
 end
