b9eb8d2f4ab6e04788df295e3606f112b5c3954d
Ajay Sharma <aj@ajsharma.com>
2014-01-21 11:48:17 -0800

    If a user mentions another user, use both of their locations.  Implements #12

--- app/models/twitter_noise_importer.rb
+++ app/models/twitter_noise_importer.rb
@@ -13,6 +13,14 @@ class TwitterNoiseImporter
           user = User.first_or_import_from_twitter_noise_user(tweet.user)
           noise = Noise.first_or_import_from_twitter_noise(tweet, user)
           noise.import_locations(user.locations)
+
+          # import locations of any mentioned users as well
+          tweet.user_mentions.each do |user_mention|
+            mentioned_user = User.where(:provider => Noise::PROVIDER_TWITTER, :provider_id => user_mention.id.to_s).first
+            if mentioned_user
+              noise.import_locations(mentioned_user.locations)
+            end
+          end
         rescue => exception
           Rails.logger.error exception
         end
