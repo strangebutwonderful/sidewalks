7e940b390616a2b7aefced43bf9a53b01bfe1059
Ajay Sharma <aj@ajsharma.com>
2014-02-09 20:07:09 -0800

    Remove expectation of twitter import on index and show

--- test/functional/noises_controller_test.rb
+++ test/functional/noises_controller_test.rb
@@ -11,16 +11,12 @@ class NoisesControllerTest < ActionController::TestCase
   end
 
   test "anyone should get index" do
-    Twitter.expects(:home_timeline).returns([])
-
     get :index
     assert_response :success
     assert_not_nil assigns(:noises)
   end
 
   test "user should get index" do
-    Twitter.expects(:home_timeline).returns([])
-
     sign_in(FactoryGirl.create(:user))
 
     get :index
@@ -29,8 +25,6 @@ class NoisesControllerTest < ActionController::TestCase
   end
 
   test "admin should get index" do
-    Twitter.expects(:home_timeline).returns([])
-    
     sign_in(FactoryGirl.create(:admin_user))
 
     get :index
