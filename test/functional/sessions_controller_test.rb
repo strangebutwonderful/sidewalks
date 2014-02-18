6b8a8a2b356d0f0bb319c64d9d674c99b72441a1
Ajay Sharma <aj@ajsharma.com>
2014-02-18 09:58:21 -0800

    Add omniauth testing for session create

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -2,14 +2,36 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
 
+  setup do 
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      :provider => 'twitter',
+      :uid => Faker::Number.number(10),
+      :info => {
+        :name => Faker::Name.name,
+        :email => Faker::Internet.email,
+        :nickname => Faker::Internet.user_name
+      }
+    })
+  end
+
   teardown do
     Rails.cache.clear
     sign_out
+    OmniAuth.config.test_mode = false
   end
 
   test "new should redirect to omniauth" do
     get :new
     assert_redirected_to '/auth/twitter'
+  end
+
+  test "create should redirect to root" do
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+
+    get :create, :provider => :twitter
+    assert_redirected_to :root
+    assert_present session[:user_id]
   end
 
   test "destroy clears session user" do
