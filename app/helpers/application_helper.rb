2467bce325a7fec4213121d6683c46dbd1903fd4
Ajay Sharma <aj@ajsharma.com>
2014-02-20 11:27:53 -0800

    Pass certain environment variables to allow client-side access

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def application_meta_tags
+    [
+      application_environment_meta_tag 
+    ].join("\n").html_safe
+  end
+
+  def application_environment_meta_tag
+    tag('meta', :name => 'environment', :content => Rails.env)
+  end
+
 end
