656274eb0a1ace4e3498961ab75b2ad67651e4ea
Ajay Sharma <aj@ajsharma.com>
2014-02-24 15:03:29 -0800

    Properly throw the exception

--- app/controllers/admin/errors_controller.rb
+++ app/controllers/admin/errors_controller.rb
@@ -4,7 +4,8 @@ class Admin::ErrorsController < Admin::AdminController
 
   # GET /admin/errors
   def index
-    raise Exception "Don't worry, this one was on purpoise ;)" 
+    # throw an exception to test exception handling on server
+    raise Exception.new "Don't worry, this one was on purpoise ;)" 
   end
 
 end
