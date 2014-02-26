ab35e90f0ea6d43d3c6b32b4c4a7ea4ad22a1324
Ajay Sharma <aj@ajsharma.com>
2014-02-25 20:57:04 -0800

    Annotations

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: trails
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  latitude   :decimal(11, 8)   not null
+#  longitude  :decimal(11, 8)   not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_trails_on_latitude   (latitude)
+#  index_trails_on_longitude  (longitude)
+#  index_trails_on_user_id    (user_id)
+#
+
 class Trail < ActiveRecord::Base
   belongs_to :user
   attr_accessible :latitude, :longitude
--- test/unit/models/trail_test.rb
+++ test/unit/models/trail_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: trails
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  latitude   :decimal(11, 8)   not null
+#  longitude  :decimal(11, 8)   not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_trails_on_latitude   (latitude)
+#  index_trails_on_longitude  (longitude)
+#  index_trails_on_user_id    (user_id)
+#
+
 require 'test_helper'
 
 class TrailTest < ActiveSupport::TestCase
