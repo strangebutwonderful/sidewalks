058d2ba9378922fd8adc69133e685e5442832c1f
Ajay Sharma <aj@ajsharma.com>
2014-02-27 13:50:15 -0800

    Set lockout as rails requests

---
+++ test/unit/models/latlng_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class LatLngTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "Blank constructor fails" do
+    assert_raises RuntimeError do
+      LatLng.new(nil, nil)
+    end
+  end
+
+  test "Constructor works with string inputs" do
+    assert_nothing_raised RuntimeError do
+      LatLng.new("0.0", "0.0")
+    end
+  end
+
+  test "Constructor works with numeric inputs" do
+    assert_nothing_raised RuntimeError do
+      LatLng.new(0, 0)
+    end
+  end
+
+  test "Constructor works with BigDecimal inputs" do
+    assert_nothing_raised RuntimeError do
+      LatLng.new(BigDecimal.new("0.0"), BigDecimal.new("0.0"))
+    end
+  end
+
+  test "Constructor works with BigDecimal inputs" do
+    assert_nothing_raised RuntimeError do
+      LatLng.new(BigDecimal.new("0.0"), BigDecimal.new("0.0"))
+    end
+  end
+
+  test "Constructor works with BigDecimal inputs" do
+    assert_nothing_raised RuntimeError do
+      LatLng.new(BigDecimal.new("0.0"), BigDecimal.new("0.0"))
+    end
+  end  
+
+end
