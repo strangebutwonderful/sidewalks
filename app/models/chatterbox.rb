eb135e72f1b96e56557535857c961acbe4d86629
Ajay Sharma <aj@ajsharma.com>
2014-04-08 18:32:32 +0000

    Basic wrapper around slack-notifier to send info on demand to slack

---
+++ app/models/chatterbox.rb
@@ -0,0 +1,23 @@
+### 
+# Chatterbox is an interface for talking to external chat services
+###
+class Chatterbox
+
+  def self.tell(message)
+    @chatterbox = Chatterbox.new
+    @chatterbox.tell message
+  end
+ 
+  def tell(message)
+    unless notifier.nil?
+      notifier.ping message
+    else
+      Rails.logger.debug "Chatterbox notifier does not exist, message was not sent"
+    end
+  end
+
+  def notifier
+    @@notifier ||= Slack::Notifier.new ENV['SLACK_TEAM_NAME'], ENV['SLACK_WEBHOOK_TOKEN'] unless ['SLACK_TEAM_NAME'] && ENV['SLACK_WEBHOOK_TOKEN'].blank?
+  end
+
+end
